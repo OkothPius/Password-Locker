39e3a244c8e7ed7eaf2309e07239db91d70de996
OkothPius <orukopius8@gmail.com>
2019-04-06 23:44:29 +0300

    Implementing the test

--- credentials.py
+++ credentials.py
@@ -13,3 +13,22 @@ class Credentials:
         self.social_account = social_account
         self.password = number
         self.email = email
+
+    credentials_account = [] # Empty credentials account
+ # Init method up here
+    def save_credentials(self):
+
+        '''
+        save_credentials method saves credentials objects into credentials_account
+        '''
+
+        Credentials.credentials_account.append(self)
+
+    def delete_credentials(self):
+
+        '''
+        delete_credentials method deletes a saved credentials from the credentials_account
+        '''
+
+        Credentials.credentials_account.remove(self)
+        
